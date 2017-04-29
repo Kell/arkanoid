7af90de5a86af911140fb7bbd10c516d049cec29
Kell <karol.gliniecki@googlemail.com>
2017-04-29 20:42:09 +0200

    added some comments

--- core/src/com/arkanoid/MapHandler.java
+++ core/src/com/arkanoid/MapHandler.java
@@ -1,6 +1,5 @@
 package com.arkanoid;
 
-import com.arkanoid.entities.Ball;
 import com.arkanoid.entities.Brick;
 import com.arkanoid.entities.Wall;
 import com.badlogic.gdx.graphics.Color;
@@ -9,29 +8,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by Kell on 29.04.2017.
+ * Class which handles all Map functionality
  */
 public class MapHandler {
 
+    // number of max possible bricks in a row
     public int maxBricksPerRow = 13;
+    // holds all bricks for current map
     public List<Brick> bricks = new ArrayList<Brick>();
 
-    Ball ball;
+    //map wall objects
     Wall wallTop;
     Wall wallLeft;
     Wall wallRight;
 
+    // bricks
     Brick brick1;
     Brick brick2;
     Brick brick3;
 
+    /**
+     * Handles everything that is needed on start up of the map
+     */
     public void initMap()
     {
+        //generate Walls
         wallTop = new Wall(0, 590, 800, 10);
         wallLeft = new Wall(0, 0, 10, 600);
         wallRight = new Wall(790, 0, 10, 600);
 
 
+        // generating bricks
+        // TODO: refactor brick generation for map
         brick1 = new Brick(10, 300, Color.BLUE);
         brick2 = new Brick(70, 300, Color.GREEN);
         brick3 = new Brick(130, 300, Color.CYAN);
@@ -40,21 +48,30 @@ public class MapHandler {
         bricks.add(brick3);
     }
 
+    /**
+     * Handles the rendering of the map
+     */
     public void render()
     {
-        wallTop.render();
-        wallLeft.render();
-        wallRight.render();
+       wallTop.render();
+       wallLeft.render();
+       wallRight.render();
 
        drawBricks();
 
     }
 
+    /**
+     * Update everything needed for the map
+     */
     public void update()
     {
 
     }
 
+    /**
+     * handles the drawing of bricks
+     */
     public void drawBricks()
     {
         //render bricks
